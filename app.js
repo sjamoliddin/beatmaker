0e2fd21b28eafb7e08d8e704d813507ec9aecdac
sjamoliddin <sjamolbek@mail.ru>
2020-12-25 21:29:13 +0500

    play sounds

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ class DrumKit {
         this.kickAudio = document.querySelector('.kick-sound');
         this.snareAudio = document.querySelector('.snare-sound');
         this.hihatAudio = document.querySelector('.hihat-sound');
+        this.isPlaying = null;
         this.bpm = 150;
         this.index = 0;
     }
@@ -15,15 +16,36 @@ class DrumKit {
         // loop over each pad
         activeBars.forEach(bar =>{
             bar.style.animation = 'playTrack .3s alternate ease-in-out 2';
+            // playing sounds
+            if(bar.classList.contains('active')){
+                if(bar.classList.contains('kick-pad')){
+                    this.kickAudio.currentTime = 0;
+                    this.kickAudio.play();
+                }
+                if(bar.classList.contains('snare-pad')){
+                    this.snareAudio.currentTime = 0;
+                    this.snareAudio.play();
+                }
+                if(bar.classList.contains('hihat-pad')){
+                    this.hihatAudio.currentTime = 0;
+                    this.hihatAudio.play();
+                }
+            }
         })
         this.index++;
     }
 
     start() {
         let interval = (60 / this.bpm) * 1000;
-        setInterval(() => {
-            this.repeat();
-        }, interval);
+        // check if it's playing
+        if(this.isPlaying){
+            clearInterval(this.isPlaying);
+            this.isPlaying = null;
+        }else{
+            this.isPlaying = setInterval(() => {
+                this.repeat();
+            }, interval);        
+        }
     }
 
     activePad() {
