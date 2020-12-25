ea5c219d64928fb080969deb0d9f9084a6c27568
sjamoliddin <sjamolbek@mail.ru>
2020-12-25 22:06:02 +0500

    add mute functionality

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ class DrumKit {
     constructor() {
         this.pads = document.querySelectorAll('.pad');
         this.playBtn = document.querySelector('.play');
+        this.muteBtns = document.querySelectorAll('.mute');
         this.kickAudio = document.querySelector('.kick-sound');
         this.snareAudio = document.querySelector('.snare-sound');
         this.hihatAudio = document.querySelector('.hihat-sound');
@@ -14,19 +15,19 @@ class DrumKit {
         let step = this.index % 8;
         const activeBars = document.querySelectorAll(`.b${step}`);
         // loop over each pad
-        activeBars.forEach(bar =>{
+        activeBars.forEach(bar => {
             bar.style.animation = 'playTrack .3s alternate ease-in-out 2';
             // playing sounds
-            if(bar.classList.contains('active')){
-                if(bar.classList.contains('kick-pad')){
+            if (bar.classList.contains('active')) {
+                if (bar.classList.contains('kick-pad')) {
                     this.kickAudio.currentTime = 0;
                     this.kickAudio.play();
                 }
-                if(bar.classList.contains('snare-pad')){
+                if (bar.classList.contains('snare-pad')) {
                     this.snareAudio.currentTime = 0;
                     this.snareAudio.play();
                 }
-                if(bar.classList.contains('hihat-pad')){
+                if (bar.classList.contains('hihat-pad')) {
                     this.hihatAudio.currentTime = 0;
                     this.hihatAudio.play();
                 }
@@ -38,18 +39,58 @@ class DrumKit {
     start() {
         let interval = (60 / this.bpm) * 1000;
         // check if it's playing
-        if(this.isPlaying){
+        if (this.isPlaying) {
             clearInterval(this.isPlaying);
             this.isPlaying = null;
-        }else{
+        } else {
             this.isPlaying = setInterval(() => {
                 this.repeat();
-            }, interval);        
+            }, interval);
         }
     }
 
     activePad() {
         this.classList.toggle('active');
+    }
+
+    updateBtn() {
+        if (!this.isPlaying) {
+            this.playBtn.classList.add('active');
+            this.playBtn.innerText = 'Stop';
+        } else {
+            this.playBtn.classList.remove('active');
+            this.playBtn.innerText = 'Start';
+        }
+    }
+
+    mute(e) {
+        const muteIndex = e.target.getAttribute('data-track');
+        e.target.classList.toggle('active');
+        if (e.target.classList.contains('active')) {
+            switch (muteIndex) {
+                case '0':
+                    this.kickAudio.volume = 0;
+                    break;
+                case '1':
+                    this.snareAudio.volume = 0;
+                    break;
+                case '2':
+                    this.hihatAudio.volume = 0;
+                    break;
+            }
+        } else {
+            switch (muteIndex) {
+                case '0':
+                    this.kickAudio.volume = 1;
+                    break;
+                case '1':
+                    this.snareAudio.volume = 1;
+                    break;
+                case '2':
+                    this.hihatAudio.volume = 1;
+                    break;
+            }
+        }
     }
 }
 
@@ -59,11 +100,18 @@ const drumKit = new DrumKit;
 
 drumKit.pads.forEach(pad => {
     pad.addEventListener('click', drumKit.activePad);
-    pad.addEventListener('animationend', function(){
+    pad.addEventListener('animationend', function () {
         this.style.animation = '';
     })
 });
 
-drumKit.playBtn.addEventListener('click', function(){
+drumKit.playBtn.addEventListener('click', function () {
+    drumKit.updateBtn();
     drumKit.start();
 });
+
+drumKit.muteBtns.forEach(muteBtn => {
+    muteBtn.addEventListener('click', function (e) {
+        drumKit.mute(e);
+    })
+})
